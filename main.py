76a1850cf18ab5f52dc09d75f86a0a8c8affeeab
Kejzin <kejzin@gmail.com>
2020-12-14 13:15:35 +0100

    Implement read_names with passing test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def read_names():
-    with open('file.txt', 'r'):
-        pass
-    return
+    with open('names.txt', 'r') as names_file:
+        names = names_file.readlines()
+        for name_index in range(len(names)):
+            names[name_index] = names[name_index].replace('\n', '')
+    return names
