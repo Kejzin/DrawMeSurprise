b80f485e65cb2f1fa49f052b7657be2be4049c21
Kejzin <kejzin@gmail.com>
2020-12-14 13:18:02 +0100

    Add names_file_name as parameter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def read_names():
-    with open('names.txt', 'r') as names_file:
+def read_names(names_file_name):
+    with open(names_file_name, 'r') as names_file:
         names = names_file.readlines()
         for name_index in range(len(names)):
             names[name_index] = names[name_index].replace('\n', '')
     return names
+
--- test_main.py
+++ test_main.py
@@ -2,5 +2,5 @@ import main
 
 
 def test_read_names():
-    names = main.read_names()
+    names = main.read_names('test_names.txt')
     assert names == ['Name1', 'Name2', 'Name3', 'Name4']
