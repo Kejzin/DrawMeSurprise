851f94637d817212f7846455c46c909225ad813d
Kejzin <kejzin@gmail.com>
2020-12-14 14:07:19 +0100

    Implement randomization to mix_names

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def read_names(names_file_name):
     with open(names_file_name, 'r') as names_file:
         names = names_file.readlines()
@@ -7,5 +9,11 @@ def read_names(names_file_name):
 
 
 def mix_names(names):
-    pair1 = [names[0], names[1]]
-    return pair1
+    pairs = []; taken = []
+    for name_index in range(0, len(names)):
+        pair = name_index
+        while pair == name_index or pair in taken:
+            pair = random.randint(0, len(names)-1)
+        taken.append(pair)
+        pairs.append((names[name_index], names[pair]))
+    return pairs
--- test_main.py
+++ test_main.py
@@ -8,4 +8,6 @@ def test_read_names():
 
 @pytest.mark.parametrize('names', [('Name1', 'Name2', 'Name3', 'Name4')])
 def test_mix_names(names):
-    assert main.mix_names(names) == ['Name1', 'Name2']
+    pairs = main.mix_names(names)
+    assert isinstance(pairs, list)
+    assert pairs[0][0] != pairs[0][1]
